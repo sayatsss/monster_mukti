caab780303bfd237688e07e64cefe754d25fbd50
Sayat samantsinghar <sayatsamantsinghar21@gmail.com>
2022-01-11 20:00:23 +0530

    Added coin generation procedural

---
+++ Assets/Scripts/Coin/CoinGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinGenerator : MonoBehaviour
+{
+
+    public List<GameObject> CoinManagers;
+    private int CoinManagerActiveValue;
+
+    private void OnEnable()
+    {
+        foreach (Transform child in transform)
+        {
+            if(child.name.Contains("Coin"))
+            {
+                CoinManagers.Add(child.transform.gameObject);
+            }
+        }
+        
+
+    }
+
+    private void Start()
+    {
+        ReArrangeCoinManagers();
+    }
+
+    private void ReArrangeCoinManagers()
+    {
+        for(int i=0;i<CoinManagers.Count;i++)
+        {
+            CoinManagers[i].SetActive(false);
+        }
+        CoinManagerActiveValue = CoinManagerValue();
+        CoinManagers[CoinManagerActiveValue].SetActive(true);
+        CoinManagers[CoinManagerActiveValue].GetComponent<CoinManager>().GenerateCoins();
+    }
+
+    private int CoinManagerValue()
+    {
+        int value=Random.Range(0, CoinManagers.Count);
+        return value;
+    }
+   
+}
---
+++ Assets/Scripts/Coin/CoinManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+    public List<GameObject> Coins=new List<GameObject>();
+    private float _coinGap = 1;
+
+    private void OnEnable()
+    {
+        foreach (Transform child in transform)
+        {
+            Coins.Add(child.transform.gameObject);
+        }
+      
+    }
+
+    public void ArrangeCoins()
+    {
+            for (int i = 0; i < Coins.Count; i++)
+            {
+
+                //Coins[i].transform.position = new Vector3(Coins[i].transform.position.x, Coins[i].transform.position.y + 0.5f, i * _coinGap);
+                Coins[i].SetActive(false);
+            }
+        
+    }
+
+    public void GenerateCoins()
+    {
+        if (Coins.Count != 0)
+        {
+            ArrangeCoins();
+            int _coinValue = Random.Range(4, Coins.Count);
+            for (int i = 0; i < _coinValue; i++)
+            {
+                 Coins[i].SetActive(true);
+            }
+        }
+    }
+
+
+  
+}
