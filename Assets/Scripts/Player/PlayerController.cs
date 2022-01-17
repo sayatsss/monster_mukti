189968cb93aec5d039ac1f0c14aa339572f0a961
Sayat samantsinghar <sayatsamantsinghar21@gmail.com>
2022-01-17 14:44:05 +0530

    Revert "Rigidbody changes to accomodate the charcatre movement and jump basic"
    This reverts commit 57896e724b6d94f2ca71d266d113addeab28cb53.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,59 +3,65 @@
 public class PlayerController : MonoBehaviour
 {
     private Animator characterAnimator;
-    private Rigidbody _rigidbody;
-
+    private CharacterController controller;
     [SerializeField]private float speed;
     [SerializeField] private float jumpValue;
     [SerializeField] private float gravity;
     [SerializeField] private float sensitivity= 3f;
-   // [SerializeField]float dead = 0.001f;
+    [SerializeField]float dead = 0.001f;
     private float _xvelocity = 0.0f;
     private float _yvelocity=0.0f;
     private Vector3 velocity;
-    private CapsuleCollider col;
-    public LayerMask groundLayers;
-
+    private float target;
+    private float moveValue;
 
     void Start()
     {
-        col = GetComponent<CapsuleCollider>();
+        controller = GetComponent<CharacterController>();
         characterAnimator = GetComponent<Animator>();
-        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
         if(GameManager.instance.GameStatus==GameManager.GameState.game.ToString())
         {
-
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-
-            Vector3 movement = new Vector3(moveHorizontal, 0, 1);
-
-            _rigidbody.velocity = movement * speed * Time.deltaTime;
-           
-            if (IsGrounded() )
+            if (controller.isGrounded)
             {
                 characterAnimator.SetBool("IsJump", false);
-                if (Input.GetKeyDown(KeyCode.Space))
+                if (Input.GetKeyDown(KeyCode.Space) || SwipeManager.swipeUp)
                 {
-                    characterAnimator.SetBool("IsJump", true);
-                    _rigidbody.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+                     moveValue = 0;
+                     characterAnimator.SetBool("IsJump", true);
+                    _yvelocity = jumpValue;
                 }
-               
+                if (Input.touchCount > 0)
+                {
+                    if (SwipeManager.swipeRight)
+                    {
+                        target = 1;
+                    }
+                    if (SwipeManager.swipeLeft)
+                    {
+                        target = -1;
+                    }
+                    moveValue = Mathf.MoveTowards(moveValue, target, sensitivity * Time.deltaTime);
+                }
+                else
+                {
+                    moveValue = (moveValue < dead) ? 0 : Mathf.MoveTowards(moveValue, 0, sensitivity * Time.deltaTime);
+                }
             }
-            
-               
-                
+            else
+            {
+                _yvelocity -= gravity;
+            }
 
-           
+           // velocity = new Vector3(moveValue, 0, 1) * speed;
+            velocity = new Vector3(Input.GetAxis("Horizontal"), 0, 1) * speed;
+
+            velocity.y = _yvelocity;
+            controller.Move(velocity * Time.deltaTime);
+
         }
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
     }
 }
