b181e00ac4b519defaff39369d67faf40d4a6390
sayatsss <sayatsamantsinghar21@gmail.com>
2022-02-23 12:29:04 +0530

    Added more cloud to accomodate the VFX action

---
+++ Assets/Scripts/Manager/WaterLevel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WaterLevel : MonoBehaviour
+{
+    
+
+    private void Update()
+    {
+        if ( GameManager.instance.GameStatus == GameManager.GameState.game.ToString())
+        {
+            this.gameObject.transform.DOMoveY(10f, 1000f);
+        }
+    }
+
+
+}
