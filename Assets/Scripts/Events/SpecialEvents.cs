cc2fa032266f068e7cfbe3e6841acf6ec3184473
sayatsss <sayatsamantsinghar21@gmail.com>
2022-02-24 15:33:00 +0530

    added camera cinamatics for thre dragon

--- Assets/Scripts/Events/SpecialEvents.cs
+++ Assets/Scripts/Events/SpecialEvents.cs
@@ -5,10 +5,14 @@ using Cinemachine;
 
 public class SpecialEvents : MonoBehaviour
 {
-    
+
+    private GameObject Dragon;
 
 
-   
+    private void Start()
+    {
+        Dragon = transform.GetComponentInParent<TilesValues>().Dragon;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
@@ -22,8 +26,10 @@ public class SpecialEvents : MonoBehaviour
     IEnumerator DragonEvent()
     {
         Time.timeScale = 0.5f;
-        //SimpleCameraShakeInCinemachine.LookAt = Garuda.transform;
-        yield return new WaitForSeconds(1f);
+       // SimpleCameraShakeInCinemachine.Instance.VirtualCamera.aim
+        SimpleCameraShakeInCinemachine.Instance.VirtualCamera.LookAt = Dragon.transform;
+        yield return new WaitForSeconds(1.5f);
+        SimpleCameraShakeInCinemachine.Instance.VirtualCamera.LookAt = CharacterStateManager.Instance.MainCharacter.transform;
         Time.timeScale = 1f;
         
     }
--- Assets/Scripts/PlatformManager/TilesValues.cs
+++ Assets/Scripts/PlatformManager/TilesValues.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class TilesValues : MonoBehaviour
 {
+
+
+    public GameObject Dragon; 
     public float TileZValue;
     public float TileYValue;
     public float TileXValue;
 
     public Vector3 RotationValue;
+
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name.Contains("Dragon"))
+            {
+                Dragon = child.gameObject;
+            }
+
+        }
+    }
 }
