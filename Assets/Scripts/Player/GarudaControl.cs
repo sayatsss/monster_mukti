16c1b9cbd520241cab5534f621d4f51c082711d5
sayatsss <sayatsamantsinghar21@gmail.com>
2022-02-25 18:14:26 +0530

    Added Floral Trees at level 4

--- Assets/Scripts/Player/GarudaControl.cs
+++ Assets/Scripts/Player/GarudaControl.cs
@@ -6,7 +6,8 @@ using DG.Tweening;
 public class GarudaControl : MonoBehaviour
 {
     public float speed;
-    private Vector3 velocity;
+    private Vector3 velocity ,cloudLODVel;
+    public GameObject aboveCloudLOD;
 
     private void Awake()
     {
@@ -24,11 +25,13 @@ public class GarudaControl : MonoBehaviour
 
     private void FixedUpdate()
     {
+        cloudLODVel= new Vector3(0, 0, 1) * speed;
         velocity = new Vector3(0, 0, 1) * speed;//curSpeed += acceleration * Time.deltaTime;
         velocity.x = Input.acceleration.x * speed;
         velocity.x = Input.GetAxis("Horizontal") * speed;
         transform.Translate(velocity  * Time.deltaTime);
-        
-        
+        aboveCloudLOD.transform.Translate(cloudLODVel * Time.deltaTime);
+
+
     }
 }
