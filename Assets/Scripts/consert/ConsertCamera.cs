9dd5c0e8b074f45a005e093b149aa27f91acee6f
sayatsss <sayatsamantsinghar21@gmail.com>
2022-03-28 14:51:41 +0530

    new animation

---
+++ Assets/Scripts/consert/ConsertCamera.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class ConsertCamera : MonoBehaviour
+{
+    public GameObject MainconsertCamera;
+    public GameObject ConsertTransition;
+
+
+}
