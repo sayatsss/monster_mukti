3bd99c26f4b1b1c2704261b89432b1abbad5cc01
sayatsss <sayatsamantsinghar21@gmail.com>
2022-04-04 12:56:16 +0530

    Sequence 2 setup

--- Assets/Scripts/Concert/MainConsertManager.cs
+++ Assets/Scripts/Concert/MainConsertManager.cs
@@ -14,21 +14,39 @@ public class MainConsertManager : MonoBehaviour
     private bool flash = false;
     static float t = 0.0f;
 
+
+
+    //TimeLine Action varibales
+
     public PlayableDirector cameraTransition;
+    public PlayableDirector BhasamasuraCameraTransition;
+    public PlayableDirector ChaanuCameraTransition;
 
 
-    //public GameObject CameraTransition;
 
 
-    [SerializeField] private Vector3 playerFinalPosition,jumpValue;
+    //Public gameobject for refence
+    [SerializeField] private GameObject VirtualCamera, IshitaCamera,CaanuCamera;
 
 
+    //Position for movement of characters.
+
+    [SerializeField] private Vector3 playerFinalPosition,jumpValue,BhasamasuraScalePosition;
+
+
+    //refence of player.
     public GameObject Player;
+    public GameObject Bhasamasura;
 
     private void Awake()
     {
         Player.SetActive(false);
-        
+       
+
+        VirtualCamera.SetActive(true);
+        IshitaCamera.SetActive(false);
+        CaanuCamera.SetActive(false);
+
 
     }
     private void Start()
@@ -53,10 +71,33 @@ public class MainConsertManager : MonoBehaviour
         Player.GetComponent<Animator>().SetBool("IsIdle", true);
         yield return new WaitForSeconds(1.5f);
         cameraTransition.Play();
-        //CameraTransition.GetComponent<TimelinePlayable>().pla
+        yield return new WaitForSeconds(20f);
+        VirtualCamera.SetActive(false);
+        IshitaCamera.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        BhasamasuraCameraTransition.Play();
+        yield return new WaitForSeconds(7f);
+        CaanuCamera.SetActive(true);
+        IshitaCamera.SetActive(false);
+        ChaanuCameraTransition.Play();
+        yield return new WaitForSeconds(1.5f);
+        StartCoroutine(BhasamasuraDisapearAction());
+
 
     }
-   
+
+
+
+    private IEnumerator BhasamasuraDisapearAction()
+    {
+        Bhasamasura.transform.DOScale(0.01f, 0.2f);
+        yield return new WaitForSeconds(0.6f);
+        Bhasamasura.SetActive(false);
+        yield return new WaitForSeconds(1.5f);
+        Bhasamasura.transform.position = BhasamasuraScalePosition;
+        Bhasamasura.SetActive(true);
+        Bhasamasura.transform.DOScale(20f, 0.5f);
+    }
     private void Update()
     {
         if (flash == true)
