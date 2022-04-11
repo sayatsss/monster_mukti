1db99054b18bf418c8a6ccf26cc1afcc6dcf4079
sayatsss <sayatsamantsinghar21@gmail.com>
2022-04-11 12:31:42 +0530

    Death Aniamtion

--- Assets/Scripts/AI/FollowAI.cs
+++ Assets/Scripts/AI/FollowAI.cs
@@ -15,7 +15,13 @@ public class FollowAI : MonoBehaviour
 
     private bool isFirstAttack = false;
 
-  
+    public int AttackCal = 0;
+
+
+    public GameObject BhasamsuraFollower;
+    //[SerializeField] private Vector3 AttackPosition;
+
+    public GameObject SoulSucker;
 
     
     
@@ -23,6 +29,7 @@ public class FollowAI : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+        SoulSucker.SetActive(false);
     }
     
    
@@ -35,8 +42,37 @@ public class FollowAI : MonoBehaviour
     }
     public void AsuraAction()
     {
-        StartCoroutine(AsuraAttack(false));
+        AttackCal += 1;
+        if(AttackCal>=3)
+        {
+            StartCoroutine(AsuraLastAttack());
+        }
+        else
+        {
+            StartCoroutine(AsuraAttack(false));
+        }
+        
     }
+
+    public IEnumerator AsuraLastAttack()
+    {
+        
+        StartCoroutine(CameraManager.instance.ChangeOfCameraAngle());
+        BhasamsuraFollower.transform.localPosition = new Vector3(0, 0, 0);
+        this.gameObject.GetComponent<Animator>().SetBool("IsAttack", true);
+        yield return new WaitForSeconds(1.8f);
+        isAttacking = true;
+
+        yield return new WaitForSeconds(0.2f);
+        StartCoroutine(SimpleCameraShakeInCinemachine.Instance.cameraAction());
+        isAttacking = false;
+        this.gameObject.GetComponent<Animator>().SetBool("IsIdle", true);
+        SoulSucker.SetActive(true);
+        StartCoroutine(GameManager.instance.GameEndAction());
+        
+
+    }
+
     public IEnumerator AsuraAttack(bool FirstAttack)
     {
        
