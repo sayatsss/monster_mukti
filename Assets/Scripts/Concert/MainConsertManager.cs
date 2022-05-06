bba5c748554530a71bec5e911818a652a31c92f9
sayatsss <sayatsamantsinghar21@gmail.com>
2022-05-06 17:54:53 +0530

    Consert changes

--- Assets/Scripts/Concert/MainConsertManager.cs
+++ Assets/Scripts/Concert/MainConsertManager.cs
@@ -146,14 +146,32 @@ public class MainConsertManager : MonoBehaviour
         }
         yield return new WaitForSeconds(2f);
         Chariot.GetComponent<PathFollowe>().moveChariot = true;
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(6f);
         chariotcameraAngles.SetActive(false);
         Bhasmasuraangle.SetActive(true);
-        yield return new WaitForSeconds(14f);
+        yield return new WaitForSeconds(8f);
         Chariot.GetComponent<PathFollowe>().moveChariot = false;
         Chariot.SetActive(false);
         Bhasmasuraangle.SetActive(false);
         LastCamera.SetActive(true);
+        yield return new WaitForSeconds(9f);
+        StartCoroutine(LastCelebration());
+
+    }
+
+    private IEnumerator LastCelebration()
+    {
+        VVLOGO.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        BoomPArticle.SetActive(true);
+        for (int i=0;i<Conffetti.Count; i++)
+        {
+            Conffetti[i].SetActive(true);
+        }
+        FireWorks.SetActive(true);
+        Blast.Play();
+
+
 
     }
 
