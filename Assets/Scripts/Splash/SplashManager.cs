e9d8716299d230ec408915f24ed2d87091742557
sayatsss <sayatsamantsinghar21@gmail.com>
2022-05-07 12:26:21 +0530

    screen resolution setup

--- Assets/Scripts/Splash/SplashManager.cs
+++ Assets/Scripts/Splash/SplashManager.cs
@@ -8,6 +8,13 @@ public class SplashManager : MonoBehaviour
     public Animator transistor;
     public float transitionTime = 1;
 
+    public GameObject SplashCanvas;
+
+
+    private void Awake()
+    {
+        CalcAspect();
+    }
     private void Start()
     {
         StartCoroutine(loadSceneTrans(1));
@@ -20,5 +27,30 @@ public class SplashManager : MonoBehaviour
         yield return new WaitForSeconds(transitionTime);
         Debug.Log("its afetr time");
         SceneManager.LoadSceneAsync(level);
+    }
+    private void CalcAspect()
+    {
+        Debug.Log("I am here");
+        float r = Camera.main.aspect;
+
+
+        
+        Debug.Log(r);
+
+        if (r > 0.6)
+        {
+
+            Debug.Log("4:3");
+            SplashCanvas.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+
+
+        }
+        else
+        {
+            SplashCanvas.transform.localScale = new Vector3(1, 1, 1);
+
+        }
+            
+       
     }
 }
